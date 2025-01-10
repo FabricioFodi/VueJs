b7fe6c3ca2460a7a6098f01d5a3a5e103cec8f94
Fabricio Fodi <fabricio_fodi@estudante.sc.senai.br>
2025-01-10 09:20:55 -0300

    Adicionando persistência de tarefa concluida

--- src/App.vue
+++ src/App.vue
@@ -1,125 +1,158 @@
 <script setup>
-import { ref } from 'vue'
+import { ref } from "vue";
 
-const tarefas = ref([])
-const novaTarefa = ref('')
+const tarefas = ref([]);
+const novaTarefa = ref("");
 
-let tentativas = localStorage.getItem('tentativas') || 0;
-if (!localStorage.getItem('token')) {
-    tentativas++;
-    localStorage.setItem('tentativas', tentativas);
-    if (tentativas > 3) {
-        alert('Você está bloqueado por tentativas indevidas!');
-        console.log('Ninguém mandou tentar burlar o sistema 👺👺');
-        window.location.hash = '/oops';
-    }
+if (!localStorage.getItem("token")) {
+  window.location.hash = "#/login";
 }
 
 //Adicionar tarefa método post
 async function adicionarTarefa() {
   try {
-
     if (!novaTarefa.value) {
-      alert('Digite uma tarefa');
+      alert("Digite uma tarefa");
       return;
     }
-    const response = await fetch('http://localhost:5183/api/tarefas', {
-      method: 'POST',
+    const response = await fetch("http://localhost:5183/api/tarefas", {
+      method: "POST",
       headers: {
-        'Authorization': `Bearer ${localStorage.getItem('token')}`,
-        'Content-Type': 'application/json'
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({ texto: novaTarefa.value, concluida: false })
+      body: JSON.stringify({ descricao: novaTarefa.value, concluida: false }),
     });
 
     if (!response.ok) {
       const dadosErro = await response.json();
-      alert(dadosErro.mensagem || 'Erro desconhecido');
-      throw new Error(dadosErro.mensagem || 'Erro desconhecido');
+      alert(dadosErro.mensagem || "Erro desconhecido");
+      throw new Error(dadosErro.mensagem || "Erro desconhecido");
     }
     const data = await response.json();
     alert(data.mensagem);
-    novaTarefa.value = '';
-
+    novaTarefa.value = "";
   } catch (erro) {
-    console.log('Erro', erro)
-    alert(erro.mensagem || 'Erro ao adicionar a tarefa')
+    alert(erro.mensagem || "Erro ao adicionar tarefa");
   }
+  listarTarefas();
 }
 
 //Listar tarefas método get
 async function listarTarefas() {
   try {
-    const response = await fetch('http://localhost:5183/api/tarefas', {
+    const response = await fetch("http://localhost:5183/api/tarefas", {
       headers: {
-        'Authorization': `Bearer ${localStorage.getItem('token')}`
-      }
-    })
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
     if (!response.ok) {
       const dadosErro = await response.json();
-      alert(dadosErro.mensagem || 'Erro desconhecido');
-      throw new Error(dadosErro.mensagem || 'Erro desconhecido');
+      alert(dadosErro.mensagem || "Erro desconhecido");
+      throw new Error(dadosErro.mensagem || "Erro desconhecido");
     }
     tarefas.value = await response.json();
   } catch (erro) {
-    console.log('Erro', erro)
-    alert(erro.mensagem || 'Erro ao listar as tarefas')
+    console.log("Erro", erro);
+    alert(erro.mensagem || "Erro ao listar as tarefas");
   }
 }
 
 //Excluir tarefa método delete
-async function excluirTarefa() {
+async function excluirTarefa(index) {
   try {
     const response = await fetch(`http://localhost:5183/api/tarefas/${index}`, {
-      method: 'DELETE',
+      method: "DELETE",
       headers: {
-        'Authorization': `Bearer ${localStorage.getItem('token')}`
-      }
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+        "Content-Type": "application/json",
+      },
     });
     if (!response.ok) {
+      const dadosErro = await response.json();
+      alert(dadosErro.mensagem || "Erro desconhecido");
+      throw new Error(dadosErro.mensagem || "Erro desconhecido");
+    }
+
+    alert("Tarefa excluída com sucesso");
+    tarefas.value = tarefas.value.filter((tarefa) => tarefa.index !== index);
+  } catch (error) {
+    console.error("Erro", error);
+  }
+
+  listarTarefas();
+}
+
+//alternar tarefa
+async function alternarTarefa(index) {
+  const tarefa = tarefas.value[index];
+
+  try{
+  //Atualizar a tarefa no backend
+  const response = await fetch(`http://localhost:5183/api/tarefas/${tarefa.id}`, {
+    method: 'PATCH',
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({concluida: !tarefa.concluida}),
+    });
+    if(!response.ok) {
       const dadosErro = await response.json();
       alert(dadosErro.mensagem || 'Erro desconhecido');
       throw new Error(dadosErro.mensagem || 'Erro desconhecido');
     }
+    tarefa.concluida = !tarefa.concluida;
 
-    alert('Tarefa excluída com sucesso');
-    tarefas.value = tarefas.value.filter(tarefa => tarefa.id !== id);
-  } catch (error) {
-    console.error('Erro', error);
-    alert(error.mensagem || 'Erro ao excluir a tarefa');
+  } catch(erro) {
+    console.error('Erro', erro);
   }
 }
 
-//alternar tarefa
-function alternarTarefa(index) {
-  tarefas.value[index].concluida = !tarefas.value[index].concluida
+async function logout() {
+  localStorage.removeItem("token");
+  window.location.hash = "#/login";
 }
-
 </script>
 
 <template>
-  <link rel="shortcut icon" href="../public/icone.ico" type="image/x-icon">
+  <link rel="shortcut icon" href="../public/icone.ico" type="image/x-icon" />
   <div>
     <h1>Lista To-Do</h1>
+    <button @click="logout">Sair</button>
 
-    <input v-model="novaTarefa" @keyup.enter="adicionarTarefa" placeholder="Adicionar nova tarefa" />
+    <input
+      v-model="novaTarefa"
+      @keyup.enter="adicionarTarefa"
+      placeholder="Adicionar nova tarefa"
+    />
     <button @click="adicionarTarefa">Adicionar</button>
+    <button @click="listarTarefas">Listar</button>
 
-    <ul>
-      <li v-for="(tarefa, index) in tarefas" :key="index">
-        <span :style="{ textDecoration: tarefa.concluida ? 'line-through' : 'none' }" @click="alternarTarefa(index)">
-          {{ tarefa.texto }}
-        </span>
-        <button @click="excluirTarefa(index)">Excluir</button>
-      </li>
-    </ul>
+    <div class="lista">
+      <ul>
+        <li v-for="(tarefa, index) in tarefas" :key="index">
+          <span
+            :style="{ textDecoration: tarefa.concluida ? 'line-through' : 'none' }"
+            @click="alternarTarefa(index)"
+          >
+            {{ tarefa.descricao }}
+          </span>
+          <button @click="excluirTarefa(tarefa.id)">Excluir</button>
+        </li>
+      </ul>
+    </div>
+    <!--Fim div lista de tarefas-->
   </div>
+  <!--Fim div principal-->
 </template>
 
 <style scoped>
 div {
-
   margin: 20px;
+}
+.lsita {
+  margin-top: 20px;
 }
 
 input {
