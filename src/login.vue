7dced7041fbd134761044a18aa27ae2b4fd65507
Fabricio Fodi <fabricio_fodi@estudante.sc.senai.br>
2025-01-16 15:07:27 -0300

    Feito layout

--- src/login.vue
+++ src/login.vue
@@ -61,16 +61,94 @@ async function login() {
     <!--Fim div Auth-links-->
   </div>
   <!--Fim div Cabecalho-->
-  <div class="login">
-    <h1>Login</h1>
-    <input type="text" v-model="usuario" placeholder="Usuário" />
-    <input type="password" v-model="senha" @keyup.enter="login" placeholder="Senha" />
-    <button @click="login">Entrar</button>
-    <p>Não tem cadastro? <a href="#/cadastro">clique aqui</a> para criar uma conta.</p>
-  </div>
+  <div class="corpo">
+    <div class="login">
+      <div class="titulo-login">
+        <h1>Login</h1>
+        <p>Entre com seu usuário e senha para acessar todos os recursos</p>
+      </div><!--Fim div titulo-login-->
+        <input type="text" v-model="usuario" placeholder="Usuário" />
+        <input type="password" v-model="senha" @keyup.enter="login" placeholder="Senha" />
+        <button @click="login">Entrar</button>
+        <div class="link-para-cadastro">
+          <p>Não tem cadastro? <a href="#/cadastro">clique aqui</a> para criar uma conta.</p>
+        </div><!--Fim div link-para-cadastro-->
+  </div><!--Fim div Login-->
+</div><!--Fim div Corpo-->
 </template>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
+
+* {
+ font-family: 'Montserrat', sans-serif;
+}
+
+.corpo {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 700px;
+}
+
+.titulo-login {
+  margin: 20px;
+}
+
+.titulo-login h1 {
+  margin-bottom: 10px;
+}
+
+.login {
+  width: 400px;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 16px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+}
+
+.login input {
+  margin: 10px 0;
+  padding: 10px;
+  width: 70%;
+  height: 35px;
+  border-radius: 16px;
+  border: 1px solid #ccc;
+  border-color: hsla(160, 100%, 37%, 1);
+  background-color: rgb(41, 41, 41);
+  color: white;
+  outline: none;
+
+}
+
+.login button {
+  margin: 10px 0;
+  padding: 5px;
+  width: 50%;
+  height: 30px;
+  border-radius: 16px;
+  border: 1px solid #000000;
+  background-color: hsla(160, 100%, 37%, 1); 
+  cursor: pointer;
+  font-weight: 700;
+}
+
+.login button:hover {
+  transform: scale(1.05);
+  transition: 0.5s;
+}
+
+.link-para-cadastro {
+  margin: 20px;
+  font-size: small;
+}
+
+.link-para-cadastro a:hover {
+  text-decoration: underline;
+}
 
 .cabecalho {
   display: flex;
