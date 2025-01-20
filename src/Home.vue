4566df57472cf046c41ec792b6074bc3d884079b
Fabricio Fodi <fabricio_fodi@estudante.sc.senai.br>
2025-01-20 16:14:43 -0300

    Formatação de texto

--- src/Home.vue
+++ src/Home.vue
@@ -1,36 +1,36 @@
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted } from "vue";
 
 // Estado de autenticação e nome do usuário
-const estaLogado = ref(!!localStorage.getItem('token'));
-const nomeUsuario = ref('');
+const estaLogado = ref(!!localStorage.getItem("token"));
+const nomeUsuario = ref("");
 
 // Função para pegar o nome do usuário
 async function pegarNomeUsuario() {
   try {
-    const response = await fetch('http://localhost:5183/api/usuario', {
+    const response = await fetch("http://localhost:5183/api/usuario", {
       headers: {
-        Authorization: `Bearer ${localStorage.getItem('token')}`,
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
       },
     });
 
     if (!response.ok) {
-      throw new Error('Erro ao pegar o nome do usuário');
+      throw new Error("Erro ao pegar o nome do usuário");
     }
 
     const data = await response.json();
     nomeUsuario.value = data.usuario; // Atualiza o nome do usuário
   } catch (error) {
-    console.error('Erro', error);
-    nomeUsuario.value = ''; // Reseta o nome do usuário em caso de erro
+    console.error("Erro", error);
+    nomeUsuario.value = ""; // Reseta o nome do usuário em caso de erro
     estaLogado.value = false; // Considera o usuário como deslogado
   }
 }
 
 // Função de logout
 async function logout() {
-  localStorage.removeItem('token');
-  nomeUsuario.value = ''; // Reseta o nome do usuário
+  localStorage.removeItem("token");
+  nomeUsuario.value = ""; // Reseta o nome do usuário
   estaLogado.value = false; // Atualiza o estado de login
 }
 
@@ -50,69 +50,77 @@ onMounted(() => {
         <div class="sign-in">
           <a v-if="!estaLogado" href="#/login">Login</a>
           <div v-else>
-            <span>Olá, {{ nomeUsuario || 'Usuário' }}</span>
-          </div><!--Fim div Else dentro da div Sign-in-->
-        </div><!--Fim div Sign-in-->
+            <span>Olá, {{ nomeUsuario || "Usuário" }}</span>
+          </div>
+          <!--Fim div Else dentro da div Sign-in-->
+        </div>
+        <!--Fim div Sign-in-->
         <div class="sign-up">
           <a v-if="!estaLogado" href="#/cadastro">Cadastrar-se</a>
           <div v-else>
-            <a @click="logout" href="#/home">Sair</a>
-          </div><!--Fim div Else dentro da div Sign-up-->
-        </div><!--Fim div Sign-up-->
-      </div><!--Fim div Auth-links-->
-    </div><!--Fim div Cabecalho-->
+            <a href="#/home" @click="logout">Sair</a>
+          </div>
+          <!--Fim div Else dentro da div Sign-up-->
+        </div>
+        <!--Fim div Sign-up-->
+      </div>
+      <!--Fim div Auth-links-->
+    </div>
+    <!--Fim div Cabecalho-->
     <div class="conteudo-principal">
       <div class="coluna">
-      <a href="#/tarefas">Lista to-do</a>
-      <a target="_blank" href="https://github.com/FabricioFodi/VueJs.git">Git Repo</a>
-      <a href="#/sobre">Sobre</a>
-      </div><!--Fim div Coluna-->
+        <a href="#/tarefas">Lista to-do</a>
+        <a target="_blank" href="https://github.com/FabricioFodi/VueJs.git">Git Repo</a>
+        <a href="#/sobre">Sobre</a>
+      </div>
+      <!--Fim div Coluna-->
       <div class="svg-icon">
-      <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+        <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
 
-      <!-- Uploaded to: SVG Repo, www.svgrepo.com, Transformed by: SVG Repo Mixer Tools -->
-      <svg
-        width="800px"
-        height="600px"
-        viewBox="-102.4 -102.4 1228.80 1228.80"
-        class="icon"
-        version="1.1"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="#000000"
-      >
-        <g id="SVGRepo_bgCarrier" stroke-width="0" />
+        <!-- Uploaded to: SVG Repo, www.svgrepo.com, Transformed by: SVG Repo Mixer Tools -->
+        <svg
+          width="800px"
+          height="600px"
+          viewBox="-102.4 -102.4 1228.80 1228.80"
+          class="icon"
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="#000000"
+        >
+          <g id="SVGRepo_bgCarrier" stroke-width="0" />
 
-        <g
-          id="SVGRepo_tracerCarrier"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke="#CCCCCC"
-          stroke-width="16.384"
-        />
-
-        <g id="SVGRepo_iconCarrier">
-          <path
-            d="M512 960c-92.8 0-160-200-160-448S419.2 64 512 64s160 200 160 448-67.2 448-160 448z m0-32c65.6 0 128-185.6 128-416S577.6 96 512 96s-128 185.6-128 416 62.4 416 128 416z"
-            fill="#ababab"
+          <g
+            id="SVGRepo_tracerCarrier"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke="#CCCCCC"
+            stroke-width="16.384"
           />
 
-          <path
-            d="M124.8 736c-48-80 92.8-238.4 307.2-363.2S852.8 208 899.2 288 806.4 526.4 592 651.2 171.2 816 124.8 736z m27.2-16c33.6 57.6 225.6 17.6 424-97.6S905.6 361.6 872 304 646.4 286.4 448 401.6 118.4 662.4 152 720z"
-            fill="#ababab"
-          />
+          <g id="SVGRepo_iconCarrier">
+            <path
+              d="M512 960c-92.8 0-160-200-160-448S419.2 64 512 64s160 200 160 448-67.2 448-160 448z m0-32c65.6 0 128-185.6 128-416S577.6 96 512 96s-128 185.6-128 416 62.4 416 128 416z"
+              fill="#ababab"
+            />
 
-          <path
-            d="M899.2 736c-46.4 80-254.4 38.4-467.2-84.8S76.8 368 124.8 288s254.4-38.4 467.2 84.8S947.2 656 899.2 736z m-27.2-16c33.6-57.6-97.6-203.2-296-318.4S184 246.4 152 304 249.6 507.2 448 622.4s392 155.2 424 97.6z"
-            fill="#ababab"
-          />
+            <path
+              d="M124.8 736c-48-80 92.8-238.4 307.2-363.2S852.8 208 899.2 288 806.4 526.4 592 651.2 171.2 816 124.8 736z m27.2-16c33.6 57.6 225.6 17.6 424-97.6S905.6 361.6 872 304 646.4 286.4 448 401.6 118.4 662.4 152 720z"
+              fill="#ababab"
+            />
 
-          <path
-            d="M512 592c-44.8 0-80-35.2-80-80s35.2-80 80-80 80 35.2 80 80-35.2 80-80 80zM272 312c-27.2 0-48-20.8-48-48s20.8-48 48-48 48 20.8 48 48-20.8 48-48 48zM416 880c-27.2 0-48-20.8-48-48s20.8-48 48-48 48 20.8 48 48-20.8 48-48 48z m448-432c-27.2 0-48-20.8-48-48s20.8-48 48-48 48 20.8 48 48-20.8 48-48 48z"
-            fill="#f25050"
-          />
-        </g>
-      </svg>
-    </div><!--Fim div SVG-->
+            <path
+              d="M899.2 736c-46.4 80-254.4 38.4-467.2-84.8S76.8 368 124.8 288s254.4-38.4 467.2 84.8S947.2 656 899.2 736z m-27.2-16c33.6-57.6-97.6-203.2-296-318.4S184 246.4 152 304 249.6 507.2 448 622.4s392 155.2 424 97.6z"
+              fill="#ababab"
+            />
+
+            <path
+              d="M512 592c-44.8 0-80-35.2-80-80s35.2-80 80-80 80 35.2 80 80-35.2 80-80 80zM272 312c-27.2 0-48-20.8-48-48s20.8-48 48-48 48 20.8 48 48-20.8 48-48 48zM416 880c-27.2 0-48-20.8-48-48s20.8-48 48-48 48 20.8 48 48-20.8 48-48 48z m448-432c-27.2 0-48-20.8-48-48s20.8-48 48-48 48 20.8 48 48-20.8 48-48 48z"
+              fill="#f25050"
+            />
+          </g>
+        </svg>
+      </div>
+      <!--Fim div SVG-->
     </div>
     <!--Fim div Conteudo Principal-->
   </div>
@@ -121,12 +129,11 @@ onMounted(() => {
 
 <style scoped>
 /* Fontes */
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap");
 
 .coluna a:hover {
   transition: 0.5s;
   color: #70f250;
-  
 }
 .cabecalho {
   display: flex;
@@ -161,7 +168,6 @@ onMounted(() => {
   font-size: 2rem;
 }
 
-
 .conteudo-principal {
   display: flex;
   flex-direction: column;
@@ -171,7 +177,7 @@ onMounted(() => {
 
 .container {
   position: relative;
-  font-family: 'Montserrat', sans-serif;
+  font-family: "Montserrat", sans-serif;
 }
 .svg-icon {
   position: fixed; /* Fixado na tela, não afeta o layout geral */
